c14932d051e64340788901c7383b72604638aeb5
Addyty <68812779+Addyty@users.noreply.github.com>
2020-07-31 01:45:17 +0530

    Update Word_2_Pdf.py

--- Word_2_Pdf.py
+++ Word_2_Pdf.py
@@ -6,6 +6,7 @@ from tkinter import filedialog
 import os
 from random import randint
 
+#Define class for creating os_window.
 class Root(Tk):
     def __init__(self):
         super(Root,self).__init__()
@@ -23,32 +24,33 @@ class Root(Tk):
         self.make_dir()
         self.button1()
 
-
+    #define Function for Button1.
     def button(self):
         self.button = ttk.Button(self.lableFrame, text = "Browse a File", command = self.fileDialog)
         self.img = PhotoImage(file="C:/Users/TYAGI/PycharmProjects/Doc2Pdf/btn1.jpg")  # make sure to add "/" not "\"
         self.button.config(image=self.img)
         self.button.grid(column =1, row = 1)
 
+    # define Function for Button2.
     def button1(self):
         self.button1 = ttk.Button(self.lableFrame, text = "Convert File", command = self.convert)
         self.img1 = PhotoImage(file="C:/Users/TYAGI/PycharmProjects/Doc2Pdf/btn2.jpg")  # make sure to add "/" not "\"
         self.button1.config(image=self.img1)
         self.button1.grid(column =1, row = 2, padx= 20, pady= 50)
 
-
+    # define Function for Dialog box for file.
     def fileDialog(self):
         self.filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetype = (("docx", "*.docx"),("All Files", "*.*")))
         self.lable = ttk.Label(self.lableFrame, text = "")
         print(self.filename)
-
+    #define function to create a new folder for output of the file.
     def make_dir(self):
         path = 'D:/Doc_2_PDF (Output)'
         try:
             os.mkdir(path)
         except OSError as error:
-            print(error)
-
+            print()
+    #define function to convert the docx file into pdf.
     def convert(self):
         i = str(randint(1,1000))
         self.input_file = self.filename
